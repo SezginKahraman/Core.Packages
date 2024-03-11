52fa3b6e72deae4a4c793b09da0d72afd7701e46
SezginKahraman <sezgin.kahraman12@gmail.com>
2024-03-11 23:10:38 +0300

    Caching behavior.

---
+++ Core.Application/Pipelines/Caching/CacheRemovingBehavior.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Core.Application.Pipelines.Caching
+{
+    public class CacheRemovingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>, ICacheRemoverRequest
+    {
+        private readonly CacheSettings _cacheSettings;
+        private readonly IDistributedCache _distributedCache;
+
+        public CacheRemovingBehavior(IDistributedCache distributedCache, IConfiguration configuration)
+        {
+            _cacheSettings = configuration.GetSection("CacheSettings").Get<CacheSettings>() ?? throw new InvalidOperationException();
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (request.BypassCache)
+                return await next();
+
+            TResponse response = await next();
+            if (!string.IsNullOrWhiteSpace(request.CacheKey))
+            { 
+                await _distributedCache.RemoveAsync(request.CacheKey, cancellationToken);
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Core.Application/Pipelines/Caching/CachingBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+
+namespace Core.Application.Pipelines.Caching
+{
+    public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>, ICachableRequest
+    {
+        private readonly CacheSettings _cacheSettings;
+        private readonly IDistributedCache _distributedCache;
+
+        public CachingBehavior(IDistributedCache distributedCache, IConfiguration configuration)
+        {
+            _cacheSettings = configuration.GetSection("CacheSettings").Get<CacheSettings>()??throw new InvalidOperationException();
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (request.BypassCache)
+                return await next();
+
+            TResponse response;
+            byte[]? cachedResponse = await _distributedCache.GetAsync(request.CacheKey, cancellationToken);
+            if (cachedResponse != null)
+            {
+                response = JsonSerializer.Deserialize<TResponse>(Encoding.Default.GetString(cachedResponse));
+            }
+            else
+            {
+                response = await getResponseAndAddToCache(request, next, cancellationToken);
+            }
+
+            return response;
+        }
+
+        private async Task<TResponse?> getResponseAndAddToCache(
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next, 
+            CancellationToken cancellationToken)
+        {
+            TResponse response = await next();
+            TimeSpan slidingExpiration = request.SlidingExpiration ?? TimeSpan.FromDays(_cacheSettings.SlidingExpiration);
+            DistributedCacheEntryOptions cacheOptions = new() { SlidingExpiration = slidingExpiration };
+
+            byte[] serializedData = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response));
+
+            await _distributedCache.SetAsync(request.CacheKey, serializedData);
+
+            return response;
+        }
+    }
+}
