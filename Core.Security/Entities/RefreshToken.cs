01816bbb23da3c875465096903403b68c669d54f
SezginKahraman <sezgin.kahraman12@gmail.com>
2024-03-14 00:28:25 +0300

    JWT Security entegration is added.

---
+++ Core.Security/Entities/RefreshToken.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Persistance.Repositories;
+
+namespace Core.Security.Entities
+{
+    public class RefreshToken : Entity<int>
+    {
+        public RefreshToken()
+        {
+            Token = string.Empty;
+            CreatedByIp = string.Empty;
+        }
+
+        public RefreshToken(int id, int userId, string token, DateTime expires, string createdByIp):base(id)
+        {
+            UserId = userId;
+            Token = token;
+            Expires = expires;
+            CreatedByIp = createdByIp;
+        }
+
+        public RefreshToken(int userId, string token, DateTime expires, string createdByIp)
+        {
+            UserId = userId;
+            Token = token;
+            Expires = expires;
+            CreatedByIp = createdByIp;
+        }
+
+        public int UserId {get; set; }
+
+        public string Token { get; set; }
+
+        public DateTime Expires {get; set; }
+
+        public string CreatedByIp { get; set; }
+
+        public DateTime? Revoked { get; set; }
+
+        public string? RevokedByIp { get; set; }
+
+        public string? ReplacedByToken { get; set; }
+
+        public string? ReasonRevoked { get; set; }
+
+        public virtual User User { get; set; } = null!;
+    }
+}
---
+++ Core.Security/Extensions/CliamsPrincipalExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.Extensions
+{
+    public static class CliamsPrincipalExtensions // this class represents user's itself
+    {
+        public static List<string>? Claims(this ClaimsPrincipal claimsPrincipal, string claimType) =>
+             claimsPrincipal?.FindAll(claimType).Select(t => t.Value).ToList();
+
+        public static List<string>? ClaimRoles(this ClaimsPrincipal claimsPrincipal) =>
+            claimsPrincipal?.Claims(ClaimTypes.Role);
+
+        public static int GetUserId(this ClaimsPrincipal claimsPrincipal) =>
+            Convert.ToInt32(claimsPrincipal?.Claims(ClaimTypes.NameIdentifier)?.FirstOrDefault());
+    }
+}
