13e42232b6bb81d63d2020b78b92aaab1d02c1cd
SezginKahraman <sezgin.kahraman12@gmail.com>
2024-03-13 20:29:32 +0300

    mssql log configurations.

---
+++ Core.CrossCuttingConcerns/Serilog/Loggers/MsSqlLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+
+namespace Core.CrossCuttingConcerns.Serilog.Loggers
+{
+    public class MsSqlLogger : LoggerServiceBase
+    {
+        private readonly IConfiguration _configuration;
+        public MsSqlLogger(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+             MsSqlConfiguration logConfig = configuration.GetSection("SerilogLogConfigurations:MsSqlConfiguration").Get<MsSqlConfiguration>() ?? throw new Exception(SerilogMessages.NullOptionsMessage);
+
+            MSSqlServerSinkOptions sinkOptions = new MSSqlServerSinkOptions()
+            {
+                TableName = logConfig.TableName,
+                AutoCreateSqlDatabase = logConfig.AutoCreateSqlTable,
+            };
+
+            ColumnOptions columnOptions = new();
+
+            global::Serilog.Core.Logger seriLogConfig = new LoggerConfiguration().WriteTo
+                .MSSqlServer(connectionString: logConfig.ConnectionString, sinkOptions: sinkOptions,
+                    columnOptions: columnOptions).CreateLogger();
+
+            Logger = seriLogConfig;
+        }
+    }
+}
